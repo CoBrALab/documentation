435fd44ecf7bccf80792748ba7ba677bdefa59a8
Gabriel A. Devenyi <gdevenyi@gmail.com>
2015-05-07 13:23:37 -0400

    Update minc-install.sh

--- minc-install.sh
+++ minc-install.sh
@@ -1,6 +1,8 @@
 #File to install minc-toolkit on neurodebian
 wget -c http://packages.bic.mni.mcgill.ca/minc-toolkit/Debian/minc-toolkit-1.0.01-20131211-Debian_7.1-x86_64.deb
 wget -c http://packages.bic.mni.mcgill.ca/minc-toolkit/Debian/bic-mni-models-0.1.1-20120421.deb
+wget -c http://ftp.us.debian.org/debian/pool/main/libj/libjpeg8/libjpeg8_8d-1+deb7u1_amd64.deb
+wget -c http://ftp.us.debian.org/debian/pool/main/t/tiff3/libtiff4_3.9.6-11_amd64.deb
 sudo apt-get update
 sudo apt-get -y --force-yes install libc6 libstdc++6 imagemagick perl freeglut3 libgl1-mesa-glx libxcb1 libxdmcp6 libx11-6 libxext6 libxau6 libuuid1 libjpeg62 libexpat1 libtiff5
 sudo dpkg -i *.deb
